6e952ab62fc2bf01fc18c8fac69ad97bd963be24
Jaimon <jamin.hogan@gmail.com>
2023-01-02 15:01:00 -0500

    created console-based functions with randomozed results

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
-/* Commenting this out to save for later
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if (topic === 'HTML') {
+
+function listTopics(){
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+function selectTopic(){
+if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
-
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
 }
+
+console.log('Here are the topics we learned through Prework:');
+listTopics ();
+console.log('Which topic should we study first?');
+selectTopic();
