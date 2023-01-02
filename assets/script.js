ed6249fe55c995b19c1b7bdc9fe582a35d8e9d87
Jaimon <jamin.hogan@gmail.com>
2023-01-02 14:36:04 -0500

    removed prepending string

--- assets/script.js
+++ assets/script.js
@@ -16,5 +16,5 @@ if (topic === 'HTML') {
 
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (var x = 0; x < topics.length; x++) {
-    console.log("Let's study", topics[x]);
+    console.log(topics[x]);
 }
